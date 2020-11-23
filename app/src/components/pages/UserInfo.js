d657da7a83f7831ae3c30f4abb51a1b7cdf3e16a
Raul Delgado <raul.delgado.diaz97@gmail.com>
2020-11-23 03:29:26 +0100

    Añadido forma de contacto simple via mailto

--- app/src/components/pages/UserInfo.js
+++ app/src/components/pages/UserInfo.js
@@ -1,24 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useLocation } from "react-router-dom";
-// import Axios from "axios";
-// import UserContext from "../../context/UserContext";
 
 export default function BookInfo() {
     const location = useLocation();
-    //const { userData } = useContext(UserContext);
-
-    useEffect(() => {
-        console.log('a');
-    });
-
-    console.log('b');
-    console.log(location.state);
     const { displayName, email } = location.state;
 
     return (
         <div>
             User: {displayName}
-            Email: {email}
+            Email:
+            <a href={"mailto:" + email}>{email}</a>
         </div>
     )
 }
