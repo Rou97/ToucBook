0304ab9c6afd3a6df22924129691fa767d6e7de9
Raul Delgado <raul.delgado.diaz97@gmail.com>
2020-11-23 18:41:31 +0100

    Rfactorizando

--- app/src/components/pages/Library.js
+++ app/src/components/pages/Library.js
@@ -18,6 +18,7 @@ export default function Library() {
                 setRes(resData);
             }
             fetchData();
+            console.log('a')
         } catch (error) {
             console.error(error);
         }
